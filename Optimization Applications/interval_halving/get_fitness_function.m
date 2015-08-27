96484857a23955085e13977347fae8e41c79835f
grantmwilliams <grant.williams@okstate.edu>
2015-08-26 22:05:06 -0500

    refactored for easier readability. added fitness functions to program

--- Optimization Applications/interval_halving/get_fitness_function.m
+++
@@ -1,6 +0,0 @@
-function [fitness_function, boundary_conditions] = get_fitness_function(input_args)
-% This function simply allows user to select which fitness function they
-% would like to use for the interval_halving test program.
-
-end
-
--- Optimization Applications/interval_halving/interval_halving.m
+++ Optimization Applications/interval_halving/interval_halving.m
@@ -7,3 +7,34 @@ function optimum_point = interval_halving(fitness_function,boundary_conditions)
 
 end
 
+function fitness = get_fitness(fitness_function,boundary_conditions,input_args )
+% Evaluates the fitness function and returns the fitness. Considered
+% boundary conditions
+if(fitness_function == 1)
+    
+    if (input_args < boundary_conditions(1) || input_args > boundary_conditions(2)) % if out of bounds
+        fitness = -1 * Inf; % arbitrarily small number to make a bad fitness
+    else
+        fitness = input_args ^ 3;
+    end % ends if
+    
+elseif (fitness_function == 2)
+    
+    if (input_args < boundary_conditions(1) || input_args > boundary_conditions(2)) % if out of bounds
+        fitness = -1 * Inf; % arbitrarily small number to make a bad fitness
+    
+    else
+        fitness = sin(input_args);
+    end % ends if
+    
+elseif (fitness_function == 3)
+    
+    if (input_args < boundary_conditions(1) || input_args > boundary_conditions(2)) % if out of bounds
+        fitness = -1 * Inf; % arbitrarily small number to make a bad fitness
+    else
+        fitness = tan(input_args)^2 / e^(input_args);
+    end % ends if
+    
+end % ends if 
+
+end % ends function
