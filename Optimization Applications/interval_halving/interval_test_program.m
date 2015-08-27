16a67346d92c9c3478a8b855a3eab846e7da0fbd
grantmwilliams <grant.williams@okstate.edu>
2015-08-26 22:30:40 -0500

    update to interval_halving. Confirmed all fitness functions work now.

--- Optimization Applications/interval_halving/interval_test_program.m
+++ Optimization Applications/interval_halving/interval_test_program.m
@@ -4,11 +4,14 @@
 %
 % Program by Grant Williams - Version 1.0.0
 
+%% Get Fitness Function
 fitness_function = 1;
 % 1 for f(x) = x^3
 % 2 for f(x) = sin(x)
 % 3 for f(x) = tan^2(x)/e^x
 
+
+%% Assign Boundary Conditions
 if (fitness_function == 1)
     boundary_conditions = [-10, 10];
     
@@ -18,3 +21,13 @@ elseif (fitness_function == 2)
 elseif (fitness_function == 3)
     boundary_conditions = [-3, 3];
 end % end fitness function boundary conditions
+
+%% Set Up Initial Guesses
+
+start_point = sum(abs(boundary_conditions)); % Take the middle point of our domain
+
+
+%% Use Function to Return Optimum Point
+optimum_point = interval_halving(fitness_function,boundary_conditions,start_point);
+
+disp(optimum_point);
