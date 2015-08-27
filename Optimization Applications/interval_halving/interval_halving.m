f6e894e7fa252bc79f083185c5841822c4befe78
grantmwilliams <grant.williams@okstate.edu>
2015-08-26 21:24:38 -0500

    added interval_halving function and test program

---
+++ Optimization Applications/interval_halving/interval_halving.m
@@ -0,0 +1,9 @@
+function optimum_point = interval_halving(fitness_function,boundary_conditions)
+% This function returns the maximum point of the fitness function within
+% the boundary conditions by use of the interval halving technique (similar
+% to binary search tree).
+
+
+
+end
+
---
+++ Optimization Applications/interval_halving/interval_test_program.m
@@ -0,0 +1,5 @@
+%% Interval Halving Test Program
+% This program is designed to test the interval halving technique with a
+% number of different functions and boundary conditions.
+%
+% Program by Grant Williams - Version 1.0.0
